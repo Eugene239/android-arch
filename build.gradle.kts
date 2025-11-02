7f39c2311d976d6204f8d5161336561a04c188a1
Evgeniy Pavlov <evgpavlov18@gmail.com>
2025-11-02 13:30:54 +0100

    Update tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,6 @@ allprojects {
     }
 }
 
-// Maven Central publication configuration via Sonatype Central Portal
-// Requirements:
-// 1. ✅ Namespace registered: io.github.eugene239 (verified)
-// 2. GPG signing configured
-// 3. Portal Token created in Central Portal (Tokens section)
-// 4. Credentials added to gradle.properties:
-//    sonatypeUsername=YOUR_USERNAME
-//    sonatypePassword=YOUR_PASSWORD
-//    signing.keyId=YOUR_GPG_KEY_ID (last 8 characters)
-//    signing.password=YOUR_GPG_PASSWORD
-//    Note: secretKeyRingFile not needed when using useGpgCmd()
-
 apply(plugin = "io.github.gradle-nexus.publish-plugin")
 
 configure<io.github.gradlenexus.publishplugin.NexusPublishExtension> {
